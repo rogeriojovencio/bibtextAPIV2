1b5ef2778d5189159314850e2f56cdd588fe32b6
Rogerio Jovencio <r.jovencio@avanade.com>
2022-05-19 17:35:57 -0300

    nova versao

--- sqlite.py
+++ sqlite.py
@@ -6,7 +6,10 @@ def sqlAPI():
     con = sqlite3.connect('example.db')
     cur = con.cursor()
     cur.execute(
-        '''create table APIModule (author, title, keywords, abstract, year, type_publication, doi)''')
+        ''' create table  if not exists 
+        APIModule (author, title, keywords, abstract, year, type_publication, doi)''')
 
     cur.execute(
         "INSERT INTO APIModule VALUES ('2006-01-05','BUY','RHAT',100,35.14,999,999)")
+    con.commit()
+    con.close()
